95ddc1b7d2d0826f4abc06809def7db4dbfc17ba
Ayushi2900 <ayushiarjariya15@gmail.com>
2026-06-29 15:56:32 +0530

    update changes

---
+++ functions.js
@@ -0,0 +1,9 @@
+function sayMyName(){
+    console.log("A");
+    console.log("Y");
+    console.log("U");
+    console.log("S");
+    console.log("H");
+    console.log("I");
+}
+sayMyName()
--- variable.js
+++ variable.js
@@ -1,1 +1,12 @@
 let a=9;
+b = 20;
+c = 25;
+console.log(a+b/2);
+console.log(b+c/2);
+console.log(c+a/2)
+
+
+// git checkout -b feature/add-a
+// git checkout feature/add-a
+// git push origin feature/add-a
+// git pull origin main
